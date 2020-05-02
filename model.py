25e7dba6066fb866daf8cebf575a9d4e870534b2
Eric Herrera <eric.herrera@hurloe.com>
2020-05-01 21:26:16 -0400

    clean migrations

--- model.py
+++ model.py
@@ -15,17 +15,8 @@ from sqlalchemy.ext.hybrid import hybrid_property
 
 Base = declarative_base()
 
-def dict_factory(cursor, row):
-  d = {}
-  for idx, col in enumerate(cursor.description):
-    d[col[0]] = row[idx]
-  return d
 
-class Account(Base):
-    __tablename__ = 'account'
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String(255), nullable=True)
+# Exchange data
 
 class Asset(Base):
     __tablename__ = 'asset'
@@ -41,21 +32,62 @@ class Market(Base):
     asset1 = Column(Integer) # fk Asset
     asset2 = Column(Integer) # fk Asset
 
-class Order(Base):
+# 
+
+class Owner(Base):
+    __tablename__ = 'owner'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255), nullable=True)
+
+class Account(Base):
+    __tablename__ = 'account'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255), nullable=True)
+    owner = Column(Integer) # fk Owner
+    asset = Column(Integer) # fk Asset
+    # amount - sum transaction table
+
+class Order(Base): # tx data; Cancel only
     __tablename__ = 'order'
 
     id = Column(Integer, primary_key=True)
-    account = Column(Integer) # fk Account
-    price = Column(Integer)
+    owner = Column(Integer) # fk Owner
+    market = Column(Integer) # fk Market
+    direction = Column(String(16)) # buy, sell
+    type = Column(String(16), default='limit') # limit, market
+    price = Column(Integer) # when market, no price
     amount = Column(Integer)
+    balance = Column(Integer)
+    status = Column(String(16), default='open')
+    # new, open, partial, done, cancel
+    # open, partial orders should be deducted from balance. It is reserved
 
-class Transaction(Base):
+class Transaction(Base): # tx data; Append only
     __tablename__ = 'transaction'
 
     id = Column(Integer, primary_key=True)
-    account1 = Column(Integer) # fk Account
-    account2 = Column(Integer) # fk Account
+    datetime = Column(DateTime)
+
+    """
+    account1 = Column(Integer) # fk Account (from)
+    asset1 = Column(Integer) # fk Asset (from)
+
+    account2 = Column(Integer) # fk Account (to)
+    asset2 = Column(Integer) # fk Asset (to)
+
     price = Column(Integer)
     amount = Column(Integer)
+    """
+
+class TransactionItem(Base): # tx data; Append only
+    __tablename__ = 'transaction_item'
+
+    id = Column(Integer, primary_key=True)
+    transaction = Column(Integer) # fk Transaction
+    account = Column(Integer) #fk Account
+    amount = Column(Integer) # signed?
+    order = Column(Integer) # fk Order
 
 
