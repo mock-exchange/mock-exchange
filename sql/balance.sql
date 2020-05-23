aa40d080452ce13e2f7260bfab536203cad1961d
Eric Herrera <eric.herrera@hurloe.com>
2020-05-22 21:39:32 -0400

    s/owner/account

--- sql/balance.sql
+++ sql/balance.sql
@@ -23,11 +23,9 @@ SELECT
     MAX(l.created) AS ending
 FROM asset AS a
 LEFT JOIN ledger AS l
-    ON a.id = l.asset_id AND l.owner_id = ?
+    ON a.id = l.asset_id AND l.account_id = ?
 LEFT JOIN value
     ON value.asset1 = a.id AND value.asset2 = 1 -- USD Asset
--- WHERE
---    owner_id = 1
 GROUP BY
     a.id
 ORDER BY
