13857bd8e4af2b05c04fc8cd235e71bf3bc7fc2d
Eric Herrera <erchrrr@pm.me>
2020-07-21 13:54:05 -0400

    reduce and anonymize

--- sql/wealth.sql
+++ sql/wealth.sql
@@ -1,8 +1,24 @@
 SELECT
-    l.account_id,
-    SUM(l.amount)
-FROM ledger AS l
+    b.ntile,
+    SUM(b.amount)::bigint AS amount
+FROM (
+    SELECT
+        l.account_id,
+        SUM(l.amount)::bigint AS amount,
+        NTILE(100) OVER (
+            ORDER BY SUM(l.amount)
+        ) AS ntile
+    FROM ledger AS l
+    WHERE
+        l.asset_id = 1
+        AND l.amount > 0
+    GROUP BY
+        l.account_id,
+        l.asset_id
+    ORDER BY
+        SUM(l.amount) DESC
+) AS b
 GROUP BY
-    l.account_id
+    b.ntile
 ORDER BY
-    SUM(l.amount) DESC
+    b.ntile DESC
